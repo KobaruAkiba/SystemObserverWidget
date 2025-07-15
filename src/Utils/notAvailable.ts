adc96b381067908add034b9b163b656a15722fac
Kobaru <ikobakitsune@gmail.com>
2025-07-16 00:02:06 +0200

    Centralized style and default strings

--- src/Utils/notAvailable.ts
+++ src/Utils/notAvailable.ts
@@ -1,12 +1,7 @@
 /**
- * Centralized value for not available text
- */
-export const notAvailableText = 'N/A';
-
-/**
  * Check if string has a not available value
  */
-export const isNotAvailableText = (val: string) => val === notAvailableText;
+export const isNotAvailableText = (val: string) => val === loadingStrings.NotAvailable;
 
 /**
  * Centralized value for not available data
@@ -17,4 +12,12 @@ export const notAvailableData = -1;
  * Checks if number has not available number
  */
 export const isNotAvailableNumber = (val: number) => val === -1;
+
+/**
+ * Centralized value for not available text
+ */
+export const loadingStrings = {
+	Dots: '...',
+	NotAvailable: 'N/A',
+};
 
