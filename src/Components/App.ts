170b56f39577f0a3e16e0fd024e5e2c8df5a2b2f
Kobaru <ikobakitsune@gmail.com>
2025-07-31 21:59:05 +0200

    Styling

--- src/Components/App.ts
+++ src/Components/App.ts
@@ -18,8 +18,13 @@ export class App extends LitElement {
 	@state() gpuLoad: number = 0;
 	@state() gpuTemperature: number = 0;
 	@state() ramLoad: number = 0;
+	@state() isSettingsPanelOpen: boolean = false;
 
-	private handleClick = () => window.sow.minimize();
+	private handleSettingPanelClick = () => {
+		this.isSettingsPanelOpen = !this.isSettingsPanelOpen;
+	};
+
+	private handleMinimizeClick = () => window.sow.minimize();
 
 	private updateStates = async () => {
 		// TODO: find a way to optimize not successfull calls (timeout after number of tries?)
@@ -41,15 +46,51 @@ export class App extends LitElement {
 				<div class="widget-move-bar"></div>
 				<span class="window-buttons">
 					<button
+						class="window-button"
+						@click=${this.handleMinimizeClick}
+					>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							class="window-button-icon"
+							viewBox="0 -960 960 960"
+						>
+							<path d="M240-120v-80h480v80H240Z" />
+						</svg>
+					</button>
+					<button
 						id="minimize-button"
 						class="window-button"
-						@click=${this.handleClick}
+						@click=${this.handleSettingPanelClick}
 					>
-						-
+						${this.isSettingsPanelOpen === true
+							? html`<svg
+									xmlns="http://www.w3.org/2000/svg"
+									class="window-button-icon"
+									viewBox="0 -960 960 960"
+								>
+									<path
+										d="M300-640v320l160-160-160-160ZM200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h560q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H200Zm440-80h120v-560H640v560Zm-80 0v-560H200v560h360Zm80 0h120-120Z"
+									/>
+								</svg>`
+							: html`<svg
+									xmlns="http://www.w3.org/2000/svg"
+									class="window-button-icon"
+									viewBox="0 -960 960 960"
+								>
+									<path
+										d="M460-320v-320L300-480l160 160ZM200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h560q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H200Zm440-80h120v-560H640v560Zm-80 0v-560H200v560h360Zm80 0h120-120Z"
+									/>
+								</svg> `}
 					</button>
 				</span>
+				${this.isSettingsPanelOpen === true
+					? html` <div class="window-side-bar">
+							<div>Options</div>
+							<div>_ Minimize</div>
+						</div>`
+					: html``}
 			</div>
-			<div class="content">
+			<div class="content ${this.isSettingsPanelOpen ? 'with-other-settings' : ''}">
 				<cpu-monitor
 					cpuLoad=${this.cpuLoad}
 					cpuTemperature=${this.cpuTemperature}
