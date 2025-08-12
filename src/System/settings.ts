4899373bc9d303a553cb996768dd02dcd839e08a
Kobaru <ikobakitsune@gmail.com>
2025-08-13 00:27:35 +0200

    Added system settings
    For managing user settings

---
+++ src/System/settings.ts
@@ -0,0 +1,54 @@
+import fs from 'fs';
+import path from 'path';
+import { app } from 'electron';
+
+type UserSettingsData = {
+	windowSize: {
+		width: number;
+		height: number;
+	};
+	refreshTicks: number;
+};
+
+const settingsPath = path.join(app.getPath('userData'), 'settings.json');
+
+const defaultSettings: UserSettingsData = {
+	windowSize: {
+		width: 380,
+		height: 200,
+	},
+	refreshTicks: 3,
+};
+
+const writeDefaultUserSettings = (): UserSettingsData => {
+	fs.writeFileSync(settingsPath, JSON.stringify(defaultSettings, null, 2));
+	return { ...defaultSettings };
+};
+
+const readSettingsFromDisk = (): UserSettingsData => {
+	if (!fs.existsSync(settingsPath)) {
+		return writeDefaultUserSettings();
+	}
+
+	try {
+		const data = JSON.parse(fs.readFileSync(settingsPath, 'utf-8'));
+		return { ...defaultSettings, ...data };
+	} catch (error) {
+		return writeDefaultUserSettings();
+	}
+};
+
+const saveSettings = (data: UserSettingsData) => {
+	console.log('Saving settings', data);
+	userSettingsCache = { ...userSettingsCache, ...data };
+	fs.writeFile(settingsPath, JSON.stringify(data, null, 2), (err) => {
+		if (err) console.error('Failed to save settings: ', err);
+	});
+};
+
+const loadSettings = (): UserSettingsData => userSettingsCache;
+
+let userSettingsCache: UserSettingsData = readSettingsFromDisk();
+
+export { UserSettingsData, loadSettings, saveSettings };
+
